4481f772e29e920facfef4af37ad49b317080a3a
Patience <peshmay@gmail.com>
2026-02-23 10:34:46 +0100

    Set up shadcn

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import { defineConfig, type PluginOption } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
+   plugins: [react(), tailwindcss()] as PluginOption[],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+   server: {
+      proxy: {
+         '/api': 'http://localhost:3000',
 
-      // /api/hello -> http://localhost:3000/api/hello
-    },
-  },
+         // /api/hello -> http://localhost:3000/api/hello
+      },
+   },
 });
